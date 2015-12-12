742789263d1b903d59fa4d38380ce55b7e18dd7f
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2015-12-12 12:26:27 +0000

    docker stats

--- example.go
+++ example.go
@@ -14,9 +14,29 @@ func main() {
 		fmt.Println(img.ID, img.RepoTags)
 	}
 	// top
-	top, _ := client.TopContainer("3ce0eefcced2", "aux")
+	top, _ := client.TopContainer("fc5bfefc175c", "aux")
 	fmt.Println(top.Titles)
 	for _, p := range top.Processes {
 		fmt.Println(p)
+	}
+	statsOption := docker.StatsOptions{}
+	stats := make(chan *docker.Stats)
+	statsOption.ID = "fc5bfefc175c"
+	statsOption.Stream = true
+	statsOption.Stats = stats
+	done := make(chan bool)
+	statsOption.Done = done
+	errC := make(chan error, 1)
+	go func() {
+		errC <- client.Stats(statsOption)
+		close(errC)
+	}()
+	for {
+		s, ok := <-stats
+		if !ok {
+			break
+		}
+		fmt.Println("Network Stats", s.Network)
+		fmt.Println("Memory Stats", s.MemoryStats)
 	}
 }
